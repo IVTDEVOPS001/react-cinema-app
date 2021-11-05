26f08267bf8128e815eb8e7bf627fdcbc13ec026
Charles Hodge <ivtdevops001@yahoo.com>
2021-11-05 10:15:19 -0400

    Charles - Attempting fix for image error.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+{/*  import logo from './logo.svg';   */}
+import logo from './selogo2.png';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+{/*  }        <img src={logo} className="App-logo" alt="logo" />    */}
+        <img src="./selogo.png" />
         <p>
           The path is clear, and the time is now.
 {/*          Edit <code>src/App.js</code> and save to reload.   */}

f2c715857bdfe7cda683c529ae6d22a3957a9974
Charles Hodge <ivtdevops001@yahoo.com>
2021-11-05 10:41:30 -0400

    Charles - ERA - Fixed mistyped opening bracket for comment.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 {/*  import logo from './logo.svg';   */}
 {/*  import logo from './selogo2.png';    */}
-(/*  import './App.css';    */}
+{/*  import './App.css';   */}
 
 const App = () => {
   return (
